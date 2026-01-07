5851cd652c0e5ebe06c104e7e1173f13d58eb644
mamielgil <m.amiel.gil@gmail.com>
2026-01-07 17:47:24 +0100

    Commit just in case before starting word analysis

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -7,8 +7,9 @@ export default function Toolbar(){
         <MenuOption onClickHandler={Model.changeConnectionMode} stringLabel= "Add connection" checked = {Model.is_connections_tool_active.value}></MenuOption>
         <MenuOption onClickHandler={Model.changeAddMode} stringLabel="Add Tool" checked = {Model.is_add_tool_active.value}></MenuOption>
         <MenuOption onClickHandler={Model.changeDeleteMode} stringLabel="Delete Tool" checked = {Model.is_delete_tool_active.value}></MenuOption>
-        <MenuOption stringLabel="Edit Node Tool" onClickHandler={Model.changeEditMode} checked = {Model.is_edit_tool_active.value}></MenuOption>
-        <button class = "ml-[15px] w-[100px]"> Save</button>
+        <MenuOption onClickHandler={Model.changeEditMode} stringLabel="Edit Node Tool" checked = {Model.is_edit_tool_active.value}></MenuOption>
+        <button class = "w-[100px]"> Analyze word</button>
+        <button class = "w-[100px]"> Save</button>
         </div>
 
 
