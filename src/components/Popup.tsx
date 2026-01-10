3b47a130c984d16e45e69a3fc209aa09cba55d4c
mamielgil <m.amiel.gil@gmail.com>
2026-01-10 18:28:13 +0100

    Minor changes

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -3,6 +3,6 @@ import * as Model from "../model"
 export function Popup({message = ""}){
     
     return(
-        Model.show_connection_popup.value ? <div class = "bg-gray-700 flex"><label class = "grow text-orange-500 text-center"> {message}</label> </div> : null
+        Model.show_connection_popup.value ? <div class = "bg-red-200 flex"><label class = "grow text-black text-center"> {message}</label> </div> : null
     );
 }
